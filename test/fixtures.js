cb2458585b436b8c4246005e7356ea16ccbbd7fe
Luca Manca <mancaluca300@gmail.com>
2020-03-28 09:49:33 +0200

    Add test fixtures

---
+++ test/fixtures.js
@@ -0,0 +1,12 @@
+exports.article_list = `
+    <body id="article_list" data-type="list">
+        <img class="logo" href="logo">
+        <h1>Page Title</h1>
+        <div class="items">
+            <a href="item1">Item 1</a>  
+            <a href="item2">Item 2</a>
+            <a href="item3">Item 3</a>
+            <a href="item4">Item 4</a>
+        </div>
+    </body>
+`;
